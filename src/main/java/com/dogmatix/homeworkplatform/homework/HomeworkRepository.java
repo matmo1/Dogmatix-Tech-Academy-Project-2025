e1fd28040a91896ead0009041afffa32f8f84c00
matmo1 <martin060607@gmail.com>
2025-02-16 14:06:13 +0200

    .

--- src/main/java/com/dogmatix/homeworkplatform/homework/HomeworkRepository.java
+++ src/main/java/com/dogmatix/homeworkplatform/homework/HomeworkRepository.java
@@ -1,6 +1,8 @@
 package com.dogmatix.homeworkplatform.homework;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface HomeworkRepository extends JpaRepository<Homework, String> {
+public interface HomeworkRepository extends JpaRepository<Homework, UUID> {
 }
